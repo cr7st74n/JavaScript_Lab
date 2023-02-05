0bc52c1cf66f17094887e27b3ad2c4458aa3d1c7
cr7st74n <cris-082011@hotmail.com>
2023-02-05 02:16:09 -0600

    adding the validation and thew first part of the class elimination

---
+++ Club_rosterM/JS/app.js
@@ -0,0 +1,42 @@
+
+//declare all the inputs and the span
+const student_count = document.querySelector("#student-count");
+
+const input_name = document.querySelector("#student-name");
+const input_id = document.querySelector("#student-id");
+const input_gpa = document.querySelector("#student-gpa");
+//declare the button 
+const btn_add = document.querySelector("#add-student");
+//declare const lits 
+const list = document.querySelector("#student-list");
+
+const List_Item = document.querySelector("li")
+
+//Initialization of the count = 0 and display it when page loads
+let countOfStu = 0;
+student_count.innerHTML = countOfStu;
+
+btn_add.addEventListener("click", ()=>{
+    if(input_name.value === "" || input_id.value == "" || input_gpa.value == ""
+     || input_gpa.value > 4 || input_gpa.value < 0  ){
+        alert("Invalid Input, try agrain");
+        return;
+     }else{
+        //appned the new student !
+        const newStudent = document.createElement("li")
+        newStudent.innerHTML = input_name.value+(" , id: ") +input_id.value+(" ,GPA: ")  +input_gpa.value;
+        list.appendChild(newStudent);
+        //add to the count of students
+        countOfStu++;
+        student_count.innerHTML = countOfStu;
+     }
+})
+
+List_Item.addEventListener("click", ()=>{
+    console.log('HOLA');
+})
+
+function SelectLi(){
+    const selected_Stu = document.querySelector("li");
+    selected_Stu.classList.toggle("selected");
+}
--- List_project/app.js
+++ List_project/app.js
@@ -7,12 +7,13 @@ const list = document.querySelector("#Team-list");
 let numOfPeople = 1;
 
 btn_submit.addEventListener("click", ()=>{
-
+    //validate if the inpout is correct to proceed with the program!
     if(input_name.value.length > 50 || input_name.value.length <1){
         alert("this text invalid! ")
         return;
     }
 
+    //if the count of people is less than 10 we can add more li elements 
     if(numOfPeople<=10){
         const new_menber = input_name.value;
         const newItem = document.createElement("li");
@@ -22,12 +23,14 @@ btn_submit.addEventListener("click", ()=>{
         list.appendChild(newItem);
     
         input_name.value= "";
+        // if we have more than 10 people, we display this alert!
     }else{
         alert("You reach the maximum, of the people for the team!")
     }
    
 })
 
+// clear the list with this function!
 btn_clear.addEventListener("click" ,()=>{
     list.innerHTML = "";
     numOfPeople = 1;
